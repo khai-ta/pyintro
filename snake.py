d8421993a2f354144efce7b41593231eb6029fa1
khai-ta <khaita2006@gmail.com>
2024-08-07 21:53:53 -0400

    Commit Message

--- snake.py
+++ snake.py
@@ -75,9 +75,9 @@ def change_direction(new_direction):
 
 def check_collisions(snake):
     x, y = snake.coordinates[0]
-    if x < 0 or x > GAME_WIDTH:
+    if x < 0 or x >= GAME_WIDTH:
         return True
-    elif y < 0 or y > GAME_HEIGHT:
+    elif y < 0 or y >= GAME_HEIGHT:
         return True
     for body_part in snake.coordinates[1:]:
         if x == body_part[0] and y == body_part[1]:
